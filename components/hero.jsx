59cfac61d0d3f880237ff889b95b5ffaa8da1f74
adk-saugat <adhikarisaugat34@gmail.com>
2025-07-23 12:31:15 -0500

    auth setup complete

--- components/hero.jsx
+++ components/hero.jsx
@@ -8,9 +8,7 @@ export default function Hero() {
   return (
     <header className="bg-darkgray rounded-xl text-white p-6 py-20 flex flex-col items-center space-y-6">
       <p className="flex flex-col items-center space-y-2">
-        <span className="text-5xl font-semibold">
-          🏡 Live Simply, Live Smart
-        </span>
+        <span className="text-5xl font-bold">🏡 Live Simply, Live Smart</span>
         <span className="text-xl">
           Manage your meals, expenses, and tasks easily.
         </span>
@@ -19,8 +17,8 @@ export default function Hero() {
         onClick={() => signIn("google")}
         className="border py-2 px-5 rounded-xl flex items-center gap-2 cursor-pointer duration-100 hover:bg-white hover:text-darkgray hover:shadow-xl"
       >
-        <FcGoogle size={20} />
-        <span className="text-lg">Sign in with Google</span>
+        <FcGoogle size={25} />
+        <span className="text-lg font-medium">Sign in with Google</span>
       </button>
     </header>
   );
--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,3 +1,5 @@
+import connectDB from "@/config/database";
+import User from "@/models/User";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
@@ -16,8 +18,24 @@ export const authOptions = {
   ],
   callbacks: {
     async signIn({ profile }) {
+      await connectDB();
+
+      const userexists = await User.findOne({ email: profile.email });
+
+      if (!userexists) {
+        const username = profile.name.slice(0, 20);
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
       return true;
     },
-    async session({ session }) {},
+    async session({ session }) {
+      const user = await User.findOne({ email: session.user.email });
+      session.user._id = user._id.toString();
+      return session;
+    },
   },
 };
